95e042bd795903ebf7b08096a99f097a91d5f031
up885000 <up885000@myport.ac.uk>
2023-11-07 21:21:46 +0000

    updated

--- app2.js
+++ app2.js
@@ -1,11 +1,16 @@
 let primaryGuns = [];
 let secondaryGuns = [];
-let perks = [];
+let perks1_2 = [];
+let perks3 = [];
+let perks4 = [];
 let tacticals = [];
 let lethals = [];
 let rules = [];
 let map = [];
-let perk = document.getElementById("perksText");
+let perk1 = document.getElementById("perk1Text");
+let perk2 = document.getElementById("perk2Text");
+let perk3 = document.getElementById("perk3Text");
+let perk4 = document.getElementById("perk4Text");
 let primary = document.getElementById("primaryText");
 let secondary = document.getElementById("secondaryText");
 let tactical = document.getElementById("tacticalText");
@@ -34,7 +39,9 @@ jQuery.get("2/equipment.txt", function (data) {
 });
 
 jQuery.get("2/perks.txt", function (data) {
-    perks = data.split(";")[0].split(",");
+    perks1_2 = data.split(";")[0].split(",");
+    perks3 = data.split(";")[1].split(",");
+    perks4 = data.split(";")[2].split(",");
 });
 
 jQuery.get("2/rules.txt", function (data) {
@@ -77,11 +84,23 @@ function randomInt(max) {
 
 // 50% chance of weapon specialist OR random pick of other perk packs
 function generatePerkPack() {
-    if (randomInt(100) >= (100 - overKillPercent)) {
-        return "Weapon Specialist"
+    if ((randomInt(100)/2) >= (100 - overKillPercent)) {
+        perk1.textContent = "Overkill"
     } else {
-        return perks[randomInt(perks.length)]
+        perk1.textContent = perks1_2[randomInt(perks1_2.length)]
     }
+    if ((perk1.textContent!="Overkill") && (randomInt(100)/2) >= (100 - overKillPercent))  {
+        perk2.textContent = "Overkill"
+    } else {
+        while (true){
+            perk2.textContent = perks1_2[randomInt(perks1_2.length)]
+            if (perk2.textContent != perk1.textContent){
+                break
+            }
+        }
+    }
+    perk3.textContent = perks3[randomInt(perks3.length)]
+    perk4.textContent = perks4[randomInt(perks4.length)]
 }
 
 // Generate throwies and populate on page
@@ -95,10 +114,10 @@ function generateLoadout() {
     if (!checkNum()) {
         return
     }
-    perk.textContent = generatePerkPack()
+    generatePerkPack()
     generateEquipment()
     primary.textContent = generatePrimary();
-    if (perk.textContent == "Weapon Specialist") {                      // Check is perk allows 2 primary guns
+    if (perk1.textContent == "Overkill" || perk2.textContent == "Overkill") {                      // Check is perk allows 2 primary guns
         secondary.textContent = generatePrimary();
         while (secondary.textContent == primary.textContent) {
             secondary.textContent = generatePrimary();
@@ -132,8 +151,8 @@ function regenerateSecondary() {
     if (!checkNum()) {
         return
     }
-    perk.textContent = generatePerkPack()
-    if (perk.textContent == "Weapon Specialist") {
+    generatePerkPack()
+    if (perk1.textContent == "Overkill" || perk2.textContent == "Overkill") {
         secondary.textContent = generatePrimary();
         while (secondary.textContent == primary.textContent) {
             secondary.textContent = generatePrimary();
